a8cc134b3c41865977b81614ea72b4ff27be0b29
kefirx1 <blazejkwiatkowski02@gmail.com>
2022-03-28 16:50:29 +0200

    env fix

--- src/main/java/pl/dev/httyd/httydplugins/database/DBConnection.java
+++ src/main/java/pl/dev/httyd/httydplugins/database/DBConnection.java
@@ -1,4 +1,6 @@
 package pl.dev.httyd.httydplugins.database;
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -6,9 +8,10 @@ import java.sql.Statement;
 
 public class DBConnection {
 
-    private final static String DBURL = System.getenv("DBURL");
-    private final static String DBUSER = System.getenv("DBUSER");
-    private final static String DBPASS = System.getenv("DBPASS");
+    private final Dotenv dotenv = Dotenv.load();
+    private final String DBURL = dotenv.get("DBURL");
+    private final String DBUSER = dotenv.get("DBUSER");
+    private final String DBPASS = dotenv.get("DBPASS");
     Connection connection;
     Statement statement;
 
